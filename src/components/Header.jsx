c6c85d242396967ea418e7001b4648c97dd42ce7
Paul7077 <npaulprasannakumar@gmail.com>
2026-08-03 13:17:56 +0530

    feat: implement responsive Header component with navigation and WhatsApp integration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { FaWhatsapp, FaBars, FaTimes, FaChevronDown } from 'react-icons/fa';
+import { SITE_CONFIG } from '../config';
 import { openGeneralWhatsApp } from '../utils/whatsapp';
 import './Header.css';
 
